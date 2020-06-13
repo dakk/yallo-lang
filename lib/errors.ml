58fdcd7769f04650b8ae35c1c934f4c0be610c37
Davide Gessa (dakk) <gessadavide@gmail.com>
2020-06-13 16:29:30 +0200

    import test

--- lib/errors.ml
+++ lib/errors.ml
@@ -1,3 +1,4 @@
 exception SyntaxError of string
 exception ParsingError of string
 exception TypeError of string
+exception SymbolNotFound of string * string
--- test/test.ml
+++ test/test.ml
@@ -43,6 +43,10 @@ let () =
       "infer", `Quick, compile true "test/const/infer.yallo" None;
       "let_infer", `Quick, compile true "test/const/let_infer.yallo" None;
     ];
+    "import", [
+      "interface", `Quick, compile true "test/import/interface.yallo" None;
+      "type", `Quick, compile true "test/import/type.yallo" None;
+    ];
     "interface", [
       "i1", `Quick, compile true "test/interface/i1.yallo" None;
       "extend", `Quick, compile true "test/interface/extend.yallo" None;
